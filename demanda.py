2b492d508108f1afce2642d3d63ff5bd068411ff
dcZIN <davicesarmorais@gmail.com>
2024-03-01 10:50:06 -0300

    Update demanda.py

--- demanda.py
+++ demanda.py
@@ -17,15 +17,27 @@ while True:
         match int(principal_input):
             
             case 1:
+                
+                
                 digitar_num_titulo = input("Digite o número ou o titulo da demanda: ")
                 if digitar_num_titulo in demanda_numero or digitar_num_titulo in demanda_titulo:
                     os.system("cls")   
                     print("00001 - Demanda titulo\nEstimativa: 0h\nHoras concluidas: 0h\nHoras restantes: 0h\n")
-                    s_n = input("Ver detalhes (s/n): ")
-                    if s_n.upper() == "S":
+                    print("1. Computar horas\n2. Ver detalhes\n3. Voltar (enter)")
+                    input_horas_ver_detalhes = input("> ")
+                    
+                    
+                    if input_horas_ver_detalhes == "1":
+                        computar_horas = input("Digite as horas: ")
+                        data_settings = input("1. Usar data automatica\n2. Digitar data manualmente\n3. Cancelar (enter)")
+                    
+                    
+                    elif input_horas_ver_detalhes == "2":
                         os.system("cls")
                         print("demandas detalhada")
                         voltar = input("Aperte enter para voltar a tela principal.")
+                
+                
                 else:                
                     print("Demanda não encontrada.")
                     time.sleep(1)
