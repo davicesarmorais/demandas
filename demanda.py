5c6aeab3f673104f1085e6062e187e3670e19a98
Davi César <davicesarmorais@gmail.com>
2024-03-02 14:37:36 -0300

    commit

--- demanda.py
+++ demanda.py
@@ -22,7 +22,7 @@ try:
 except FileNotFoundError:
     demandas = {}
     with open("demandas.json", 'w') as f:
-        json.dump(demandas, f, indent="4") 
+        json.dump(demandas, f, indent=4) 
 
 while True:
     
@@ -85,10 +85,42 @@ while True:
                     print("Demanda não encontrada.")
                     time.sleep(1)
 
+            
             case 2:
-                cadastrar_numero = int(input("Digite o número da demanda: "))
-                cadastrar_titulo = input("Digite o titulo da demanda: ")
-                cadastrar_estimativa = float(input("Digite a estimativa de horas da demanda: "))
+                os.system("cls")
+                print("1. Adicionar demanda\n2. Remover demanda\n3. Editar demanda\n4. Voltar (enter)")
+                editar_demanda = input("> ")
+                match editar_demanda:
+                    case "1":
+                        os.system("cls")
+                        print("Para voltar a qualquer momento aperte 'Enter'\n")
+                        
+                        cadastrar_numero = input("Digite o NÚMERO da demanda: ")
+                        if cadastrar_numero != "":
+                            
+                            cadastrar_titulo = input("Digite o TÍTULO da demanda: ")
+                            if cadastrar_titulo != "":
+                                
+                                cadastrar_estimativa = int(input("Digite a estimativa de horas da demanda: "))
+                                if str(cadastrar_estimativa) != "":
+                                    
+                                    if cadastrar_numero not in demandas:
+                                        cadastro_dict = {}
+                                        cadastro_dict[cadastrar_numero] = {"titulo": cadastrar_titulo, "estimativa": cadastrar_estimativa, "datas": {}}
+                                        demandas.update(cadastro_dict)
+                                        with open("demandas.json" , "w") as f:
+                                            json.dump(demandas , f, indent=4) # atualizar lista
+                                        print(f"Demanda ({cadastrar_numero}) adicionada com sucesso!")
+                                        time.sleep(2)
+                                    
+                                    else:
+                                        print("Não foi posssível cadastrar essa demanda (Há outra com o mesmo número registrada).")
+                                        time.sleep(3)
+                    case "2":
+                        remover_numero = input("Digite o número da demanda que deseja remover: ")
+            
+            
+            
             case 3:
                 os.system("cls")
                 
@@ -122,6 +154,6 @@ while True:
                         print(f"Status: concluída")
                         print(f"Horas gastas: {soma}h")
                         print(f"Horas restantes: {diferenca}h")
-                        print("-" * 10)
+                        print("-" * 20)
                 print()
                 voltar = input("Aperte enter para voltar a tela principal.")
