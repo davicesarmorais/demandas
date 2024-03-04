768ea13361945d9c4f238f78c96a1c6345fba9f5
Davi César <davicesarmorais@gmail.com>
2024-03-03 23:41:48 -0300

    update

--- demanda.py
+++ demanda.py
@@ -62,7 +62,7 @@ while True:
                         
                         if input_horas_ver_detalhes == "1": # Computar horas
                             computar_horas = input("Digite as horas: ")
-                            if not computar_horas == "":
+                            if computar_horas != "" and computar_horas.isdigit():
                                 print("1. Usar data automatica\n2. Digitar data manualmente\n3. Cancelar (enter)")
                                 data_settings = input("> ")
                                 
@@ -87,6 +87,9 @@ while True:
                                     print("Horas computadas com sucesso!")
                                     time.sleep(1)
                                     break
+                            else:
+                                print("Formato inválido.")
+                                time.sleep(1)
                         
                         elif input_horas_ver_detalhes == "2": # Ver detalhes
                             os.system("cls")
@@ -98,13 +101,13 @@ while True:
                             print(f"Horas restantes: {diferenca}h") 
                             for item in lista:
                                 print(f"{item[0]}: {item[1]}h")
-                            print()
+                            
                             print("\n1. Computar horas\n2. Ver menos detalhes\n3. Voltar (enter)")
                             input_horas_ver_detalhes = input("> ")
                             
                             if input_horas_ver_detalhes == "1": # Computar horas
                                 computar_horas = input("Digite as horas: ")
-                                if not computar_horas == "":
+                                if computar_horas != "" and computar_horas.isdigit():
                                     print("1. Usar data automatica\n2. Digitar data manualmente\n3. Cancelar (enter)")
                                     data_settings = input("> ")
                                     
@@ -118,6 +121,7 @@ while True:
                                         print("Horas computadas com sucesso!")
                                         time.sleep(1)
                                         break
+
                                     
                                     elif data_settings == "2":
                                         data_input = input("Digite a data (formato: dia/mes/ano): ")
@@ -129,8 +133,11 @@ while True:
                                         print("Horas computadas com sucesso!")
                                         time.sleep(1)
                                         break
+                                else:
+                                    print("Formato inválido.")
+                                    time.sleep(1)
                             
-                            elif input_horas_ver_detalhes == "2":
+                            elif input_horas_ver_detalhes == "2": # ver menos detalhes
                                 os.system("cls")
                             else:
                                 break
@@ -143,50 +150,60 @@ while True:
 
             
             case 2:
-                os.system("cls")
-                print("1. Adicionar demanda\n2. Remover demanda\n3. Editar demanda\n4. Voltar (enter)")
-                editar_demanda = input("> ")
-                match editar_demanda:
-                    case "1":
-                        os.system("cls")
-                        print("Para voltar a qualquer momento aperte 'Enter'\n")
-                        
-                        cadastrar_numero = input("Digite o NÚMERO da demanda: ")
-                        if cadastrar_numero != "":
+                while True:
+                    os.system("cls")
+                    print("1. Adicionar demanda\n2. Remover demanda\n3. Editar demanda\n4. Voltar (enter)")
+                    editar_input = input("> ")
+                    match editar_input:
+                        case "1":
+                            os.system("cls")
+                            print("Para voltar a qualquer momento aperte 'Enter'\n")
+                            cadastrar_numero = input("Digite o NÚMERO da demanda: ")
                             
-                            cadastrar_titulo = input("Digite o TÍTULO da demanda: ")
-                            if cadastrar_titulo != "":
+                            if cadastrar_numero != "":
+                                cadastrar_titulo = input("Digite o TÍTULO da demanda: ")
                                 
-                                cadastrar_estimativa = int(input("Digite a estimativa de horas da demanda: "))
-                                if str(cadastrar_estimativa) != "":
+                                if cadastrar_titulo != "":    
+                                    cadastrar_estimativa = input("Digite a estimativa de horas da demanda: ")
                                     
-                                    if cadastrar_numero not in demandas:
-                                        cadastro_dict = {}
-                                        cadastro_dict[cadastrar_numero] = {"titulo": cadastrar_titulo, "estimativa": cadastrar_estimativa, "datas": {}}
-                                        demandas.update(cadastro_dict)
-                                        with open("demandas.json" , "w") as f:
-                                            json.dump(demandas , f, indent=4) # atualizar lista
-                                        print(f"Demanda ({cadastrar_numero}) adicionada com sucesso!")
-                                        time.sleep(2)
-                                    
+                                    if str(cadastrar_estimativa) != "" and cadastrar_estimativa.isdigit():    
+                                        if cadastrar_numero not in demandas:
+                                            
+                                            cadastro_dict = {}
+                                            cadastro_dict[cadastrar_numero] = {"titulo": cadastrar_titulo, "estimativa": int(cadastrar_estimativa), "datas": {}}
+                                            demandas.update(cadastro_dict)
+                                            with open("demandas.json" , "w") as f:
+                                                json.dump(demandas , f, indent=4) # atualizar lista
+                                            print(f"Demanda ({cadastrar_numero}) adicionada com sucesso!")
+                                            time.sleep(2)
+                                            break
+                                        else:
+                                            print("Não foi posssível cadastrar essa demanda (Há outra com o mesmo número registrada).")
+                                            time.sleep(3)
                                     else:
-                                        print("Não foi posssível cadastrar essa demanda (Há outra com o mesmo número registrada).")
-                                        time.sleep(3)
-                    case "2":
-                        remover_demanda = input("Digite o número da demanda que deseja remover: ")
-                        if remover_demanda not in demandas:
-                            print("Demanda não encontrada.")
-                        else:
-                            print(f"Você realmente deseja excluir a demanda {remover_demanda} (s/n)")
-                            confirmacao = input("> ")
-                            if confirmacao.upper() == "S":
-                                del demandas[remover_demanda]
-                                with open("demandas.json" , "w") as f:
-                                    json.dump(demandas , f, indent=4)
-                                print(f"Demanda {remover_demanda} excluída com sucesso.")
-                                time.sleep(1)
-                    case "3":
-                        ...
+                                        print("Operação cancelada ou formato inválido.")
+                                        time.sleep(1)
+                        case "2":
+                            remover_demanda = input("Digite o número da demanda que deseja remover: ")
+                            if remover_demanda != "":
+                                if remover_demanda not in demandas:
+                                    print("Demanda não encontrada.")
+                                    time.sleep(1.5)
+                                else:
+                                    print(f"Você realmente deseja excluir a demanda {remover_demanda}? (s/n)")
+                                    confirmacao = input("> ")
+                                    if confirmacao.upper() == "S":
+                                        del demandas[remover_demanda]
+                                        with open("demandas.json" , "w") as f:
+                                            json.dump(demandas , f, indent=4)
+                                        print(f"Demanda {remover_demanda} excluída com sucesso.")
+                                        time.sleep(1)
+                        case "3":
+                            editar_demanda = input("Digite o número da demanda que quer editar")
+                            if editar_demanda not in demandas:
+                                print("Demanda não encontrada")
+                        case "":
+                            break
 
                     
             
